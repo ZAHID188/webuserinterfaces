098d8dd2b21caad5044851ad20176a477b7be958
zahidul_alam_pranta <zahidpranta@gmail.com>
2021-04-05 22:16:56 -0700

    in the index we used unlimited scroll post

---
+++ scroll/post/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse ,JsonResponse
+import time
+# Create your views here.
+def index(request):
+    return render(request, "post/index.html")
+def posts(request):
+
+    start = int(request.GET.get("start") or 0)
+    end = int(request.GET.get("end") or (start + 9))
+    
+    data=[]
+    for i in range(start, end + 1):
+        data.append(f"Post #{i}")
+   
+    time.sleep(1)
+    return JsonResponse({
+        "posts":data
+    })
+   
+
+        
+
+  
+    
