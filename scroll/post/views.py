808c3292052b2035f64d14ad3fa310591deb5bc2
zahidul_alam_pranta <zahidpranta@gmail.com>
2021-04-06 23:52:55 -0700

    animate. html in scroll ,app- post

--- scroll/post/views.py
+++ scroll/post/views.py
@@ -5,15 +5,18 @@ import time
 def index(request):
     return render(request, "post/index.html")
 def posts(request):
-
+   # Get the start point
     start = int(request.GET.get("start") or 0)
     end = int(request.GET.get("end") or (start + 9))
     
+
+    #Generate list of posts
     data=[]
     for i in range(start, end + 1):
         data.append(f"Post #{i}")
-   
+   # Artificially delay speed of response
     time.sleep(1)
+    #return list of posts
     return JsonResponse({
         "posts":data
     })
